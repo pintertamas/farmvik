d6d8d04c2dc6155d62e7df7ea8c7fa1e9871d82e
Tomi <pintertamas99@gmail.com>
2018-10-23 22:08:11 +0200

    ł

--- main.c
+++ main.c
@@ -8,29 +8,45 @@
 #include <time.h>
 #include "global.h"
 
-int main(int argc, char *argv[]) {
+int main( int argc, char *argv[] ) {
 
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init( SDL_INIT_EVERYTHING );
 
-    SDL_Window *window = SDL_CreateWindow("Farmville", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
+    {
+        printf( "Error: " );
+        printf( SDL_GetError( ) );
+    }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Window *window = SDL_CreateWindow( "Farmville", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN );
 
-    SDL_SetRenderDrawColor(renderer, 100, 200, 55, 155);
+    if( NULL == window )
+    {
+        printf( "Error: " );
+        printf( SDL_GetError( ) );
 
-    SDL_RenderClear(renderer);
+        return EXIT_FAILURE;
+    }
 
-    SDL_RenderPresent(renderer);
+    SDL_Renderer *renderer = SDL_CreateRenderer( window, -1, 0 );
+
+    SDL_SetRenderDrawColor( renderer, 100, 200, 55, 155 );
+
+    SDL_RenderClear( renderer );
+
+    SDL_RenderPresent( renderer );
+
+    SDL_Delay(1000);
+
+    SDL_Event windowEvent;
 
     bool running = true;
 
-    SDL_Event event;
-
     while( running )
     {
-        while( SDL_PollEvent( &event ) != 0 )
+        while( SDL_PollEvent( &windowEvent ) != 0 )
         {
-            if( event.type == SDL_QUIT )
+            if( windowEvent.type == SDL_QUIT )
             {
                 running = false;
                 break;
@@ -45,6 +61,5 @@ int main(int argc, char *argv[]) {
 
 
 
-
-    return 0;
+    return EXIT_SUCCESS;
 }
